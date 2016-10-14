c8d1e1e17ac959c3fb97ac427ae5bf19b77526b8
Alex Kafer <me@alexkafer.com>
2016-10-13 20:03:11 -0500

    Added documentation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,11 +11,21 @@
         'ngRoute'
     ])
     .config(['$routeProvider', function ($routeProvider) {
+
+            // Connects the editor.html file with the sequenceController on the main directory.
             $routeProvider.when('/', {
                 templateUrl: _templateBase + '/sequence/editor.html' ,
                 controller: 'sequenceController',
                 controllerAs: '_ctrl'
             });
+
+            // Here is an example on how to create additional pages or interfaces within the application.
+            // $routeProvider.when('/path/to/new/window', {
+            //     templateUrl: _templateBase + '/path/to/html/template' ,
+            //     controller: 'controllerName',
+            //     controllerAs: '_ctrl'
+            // });
+
             $routeProvider.otherwise({ redirectTo: '/' });
         }
     ]);
